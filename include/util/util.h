6889144fe1619390d797f56fcf62c587aefe1748
Minh Vu <minhvh99@gmail.com>
2025-03-01 13:34:54 +0700

    fix: put templated function def in header file

--- include/util/util.h
+++ include/util/util.h
@@ -2,4 +2,4 @@
 
 #include <vector>
 
-template <class T> size_t sizeof_vec_data(const std::vector<T>& v);
+template <class T> size_t sizeof_vec_data(const std::vector<T>& v) { return sizeof(T) * v.size(); }
--- src/util/util.cpp
+++
@@ -1,3 +0,0 @@
-#include "util/util.h"
-
-template <class T> size_t sizeof_vec_data(const std::vector<T>& v) { return sizeof(T) * v.size(); }
